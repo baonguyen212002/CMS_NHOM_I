248aaac0dd6c66bfc76771d3bc6e1eed5ab3ff93
baonguyen212002 <baonguyen212002@gmail.com>
2023-10-22 20:59:51 +0700

    5-search-result

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -11,8 +11,14 @@
 
 get_header();
 
-if (have_posts()) {
 	?>
+	<style>
+body {
+    background: url(http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/img/bg_pattern.png) repeat 	;
+    font-family: 'Open Sans', sans-serif;
+}
+
+	</style>
 	<div class="row">
 		<div class="col-md-3">
 			<h5>Module 13</h5>
@@ -21,6 +27,7 @@ if (have_posts()) {
 		</div>
 		<div class="col-md-6">
 			<div class="container">
+				<?php if (have_posts()) {?>
 				<header class="page-header alignwide">
 					<h1 class="page-title">
 						<?php
@@ -49,6 +56,30 @@ if (have_posts()) {
 					);
 					?>
 				</div><!-- .search-result-count -->
+				<?php
+						// Start the Loop.
+						while (have_posts()) {
+							the_post();
+
+							/*
+							* Include the Post-Format-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							*/
+							get_template_part('template-parts/content/content-excerpt-search', get_post_format());
+							
+						} // End the loop.
+
+						// Previous/next page navigation.
+						twenty_twenty_one_the_posts_navigation();
+
+						// If no content, include the "No posts found" template.
+					} else {
+						get_template_part('template-parts/content/content-none');
+					}
+
+
+					?>
 			</div>
 		</div>
 		<div class="col-md-3">
@@ -72,25 +103,5 @@ if (have_posts()) {
 			</div>
 		</div>
 	</div>
-	<?php
-	// Start the Loop.
-	while (have_posts()) {
-		the_post();
-
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part('template-parts/content/content-excerpt', get_post_format());
-	} // End the loop.
-
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
-
-	// If no content, include the "No posts found" template.
-} else {
-	get_template_part('template-parts/content/content-none');
-}
-
-get_footer();
+	
+<?php get_footer(); ?>
